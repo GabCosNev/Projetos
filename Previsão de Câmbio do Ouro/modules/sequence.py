b2a701839a9e89500a7eace3961ccad0890df033
Gabriel Costa <nevesgabriel10@gmail.com>
2025-01-13 10:53:25 -0300

    Revisão

--- Previsão de Câmbio do Ouro/modules/sequence.py
+++ Previsão de Câmbio do Ouro/modules/sequence.py
@@ -2,8 +2,7 @@ import numpy as np
 
 def create_sequences(data, T):
     X = []
-    Y = []
-    D = 1
+    y = []
 
     # Loop para criar sequências a partir de 'data' com janela de tamanho T
     # O loop vai até 'len(data) - T' para evitar acessar índices fora do limite
@@ -11,13 +10,13 @@ def create_sequences(data, T):
         # Adiciona a sequência de entrada (T valores consecutivos de data)
         X.append(data[t:t + T])
         # Adiciona o valor seguinte à sequência como a saída (Y)
-        Y.append(data[t + T])
-    
-    # N é o número de sequências geradas
-    N = len(X)
+        y.append(data[t + T])
     
     # Retorna X como um array NumPy de forma adequada, Y como array, e N como o número de sequências
-    return np.array(X).reshape(-1, T, D), np.array(Y), N
+    X = np.array(X).reshape(-1, T)  # Formato [amostras, timesteps, features]
+    y = np.array(y).reshape(-1, 1)     # Formato [amostras, 1] para saída
+    
+    return X, y
 
 
 
