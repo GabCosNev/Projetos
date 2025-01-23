0c2a148f7ab55f4e7f270808eae9811b7b3cff91
Gabriel Costa <nevesgabriel10@gmail.com>
2025-01-23 15:14:28 -0300

    att

--- Previsor de Performace de Estudantes/sequence/meanCalculator.py
+++
@@ -1,9 +0,0 @@
-def categorizar_performance(media):
-    if 0 <= media <= 5:
-        return 1
-    elif 6 <= media <= 10:
-        return 2
-    elif 11 <= media <= 15:
-        return 3
-    else:
-        return 4
