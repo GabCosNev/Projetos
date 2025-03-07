f15834a7892f32dad11eb369b6bb3015721b43b2
Gabriel Costa <nevesgabriel10@gmail.com>
2025-03-07 12:27:30 -0300

    Finalizado

--- Analise de Obesidade/sequence/replace_string.py
+++
@@ -1,9 +0,0 @@
-def replace_string_to_sequence(data, column):
-    mapping = {
-        'no': 1,
-        'Sometimes': 2,
-        'Frequently': 3,
-        'Always': 4
-    }
-    data[column] = data[column].map(mapping)
-    return data
